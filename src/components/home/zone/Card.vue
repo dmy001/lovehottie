2a204afc0d2d742e4b9da64db5c08d9f356af991
zhangzhe <zeosky@163.com>
2021-04-01 16:08:11 +0800

    组件目录更改   动态广场编辑

---
+++ src/components/home/zone/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="zone-card-container border-card-gray m-px10">
+    <div class="p-px10 w-full">
+      <div class="flex items-center">
+        <img
+          class="w-px30 h-px30 rounded-1/2"
+          src="https://images.gagahi.com/A-942e2ce4e071d89e7d2afc0ab59d9416"
+        />
+        <div class="flex flex-col text-12px">
+          <span class="text-base-color2">Jack</span>
+          <span class="text-base-color4">中国</span>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="w-full"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.zone-card-container {
+  width: 260px;
+}
+</style>
--- src/views/zone/Index.vue
+++ src/views/zone/Index.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>动态空间</div>
+  <div>
+    动态空间
+    <div class="w-full h-px50 bg-white flex items-center">
+      <span class="m-px15">动态广场</span>
+      <span class="m-px15">朋友圈</span>
+    </div>
+  </div>
 </template>
