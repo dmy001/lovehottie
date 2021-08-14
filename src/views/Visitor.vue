fbf004d0d209995f9ff2b80ca04e8af10d024756
lichengyang <lichengyang@zoneyet.com@>
2021-08-14 18:19:35 +0800

    /访客

---
+++ src/views/Visitor.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="w-full ">
+    <div class="w-full bg-white border border-opacity-0 pl-5">
+      <div class=" text-left mt-5">
+        <span> 总浏览量:8</span>
+        <span> 今日浏览量:8</span>
+      </div>
+      <div class="mt-10">
+        <div class="time-content border-dashed w-24 border rounded-3xl">
+          2021.08.13
+        </div>
+        <div class="visitors border-l-2 ml-12">
+          <div class=" flex ">
+            <div class="w-20 ml-10">
+              <img
+                class="h-20 w-20 rounded-full"
+                src="https://sources.lovehottie.com/images/default/male.png"
+                alt=""
+              />
+              <span>Eric </span>
+              <p>2021</p>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.time-content {
+  height: 26px;
+  line-height: 26px;
+}
+</style>
