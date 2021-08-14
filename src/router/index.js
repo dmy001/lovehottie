2ee0c0b03c2bea1c5ca188c85b0e4db978fcda9c
蔡文生 <caiwensheng@zoneyet.com>
2021-08-14 18:01:26 +0800

    朋友圈 和 动态广场页面编写

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,9 @@ const Zone = () => import('@/views/zone/Index.vue')
 const ZoneRoom = () => import('@/views/zone/Room.vue')
 const ZoneFriend = () => import('@/views/zone/Friend.vue')
 const Person = () => import('@/views/Person/index.vue')
+const Dynamic =() => import('@components/editDynamic.vue')
+
+
 
 
 // 避免同一个路由多次添加报错
@@ -29,6 +32,12 @@ const routes = [
       loginCheck: false,
       title: '首页'
     }
+  },
+  {
+path:'/dynamic',
+name:'dynamic',
+component:Dynamic,
+
   },
   {
     path: '/about',
