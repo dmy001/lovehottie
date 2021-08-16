f4e4b521b30583f89e7ef32de7056362363fe0ee
lichengyang <lichengyang@zoneyet.com@>
2021-08-16 14:00:09 +0800

    /升级会员

--- src/views/vip/UpgradeVip.vue
+++ src/views/vip/UpgradeVip.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-    
     <div class="flex">
         <vip/>
     </div>
+    <PayInfo/>
+    <Discount/>
   </div>
 </template>
 
 <script>
 import vip from '@components/vip/index.vue'
+import PayInfo from '@components/vip/PayInfo/PayInfo.vue'
+import Discount from '@components/vip/PayInfo/Discount.vue'
 
 export default {
     components:{
         vip,
+        PayInfo,
+        Discount
     }
 };
 </script>
