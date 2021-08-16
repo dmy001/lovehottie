d36c9a267626688866199030d5ba0401fbaa2a29
蔡文生 <caiwensheng@zoneyet.com>
2021-08-16 18:01:35 +0800

    朋友圈和动态广场样式修改

--- src/views/zone/Room.vue
+++ src/views/zone/Room.vue
@@ -1,17 +1,38 @@
 <template>
-  <div class="w-full">
-    <!-- <Card /> -->
-    <waterfall />
+  <div class="relative ">
+    <div class="personContent">
+      <div class="shareList flex justify-start">
+     
+    
+        <waterfall />
+      </div>
+     
+    </div>
+    <!-- 图片弹出框 -->
+    <Modal
+      v-model="modalPhoto"
+      title="上传图片"
+      width="600"
+      footer-hide
+      @on-cancel="cancel"
+    >
+      <p slot="header">
+        <span class="text-white">上传图片</span>
+      </p>
+    </Modal>
   </div>
+
 </template>
 
 <script>
 // import Card from "@components/home/zone/Card.vue";
 import waterfall from "@components/home/zone/Waterfall.vue";
+
 export default {
   name: "zoneCard",
   components: {
     waterfall,
+
   },
   created() {
     this.$store.commit("setCurrentPath", {
