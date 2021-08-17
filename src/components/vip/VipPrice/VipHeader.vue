bc8e0eaa4774c4ce6f8dcc95fd688b3d83315e03
lichengyang <lichengyang@zoneyet.com@>
2021-08-17 18:13:02 +0800

    /修改升级会员页面结构

--- src/components/vip/VipPrice/VipHeader.vue
+++ src/components/vip/VipPrice/VipHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="align-middle mt-5 mb-5">
-      <i class="headerImg inline-block align-middle"></i>
+      <i ref="bg" class="headerImg1 inline-block align-middle "></i>
       <span class="text-2xl align-middle">{{title}}</span>
   </div>
 </template>
@@ -11,18 +11,29 @@ export default {
       title:{
         type:String,
         defalut:'Mini会员'
+      },
+      y:{
+        type:Number,
       }
     },
     data(){
-      return {}
+      return {
+               
+      }
+    },
+    mounted(){
+      var bg = this.$refs.bg;
+      bg.style.backgroundPosition="'-75px'+this.y+'px'"
     }
 }
 </script>
 
 <style lang ='scss' scoped>
-.headerImg{
+.headerImg1{
   width: 30px;
   height: 30px;
    background: url("~@images/person/little1.png") -75px -1055px no-repeat;
 }
+
+
 </style>
