014258d4b17b9d742b8e0e8bb3752ddac93abfa3
蔡文生 <caiwensheng@zoneyet.com>
2021-08-20 18:08:57 +0800

    语言功能

--- src/views/vip/RewardRecord.vue
+++ src/views/vip/RewardRecord.vue
@@ -1,26 +1,26 @@
 <template>
 <div>
   <section >
-    <div class="reward">
-      <div class="rewardLayout">
-        <div class="rewardtop flex justify-end text-right">
+    <div >
+      <div class=" mt-5 mb-10" style="width:796px; height:500px">
+        <div class="rewardtop text-sm flex justify-end text-right mt-14  mb-4 " style=" width:740px;">
           
           <span @click="meReward" :class="{ 'text-red-500': RewardPage == false }" >我的打赏</span>
-          <span @click="meReward" >我的打赏</span>
+     
           <span >/</span>
           <span  @click="ReceiveReward" :class="{ 'text-red-500': RewardPage == true }">收到的打赏</span>
         </div>
      <table >
-<thead class="tableSize">
+<thead class=" h-full" style=" width: 745px">
   <!-- 表格头部 -->
-  <tr class="rewardformtop">
- <th class="tableIndication">指示</th>
- <th class="tableGold">打赏金币</th>
- <th class="tableTime">打赏时间</th>
+  <tr>
+ <th class=" h-10 border-red-500 bg-blue-100 border" style="width:301px:height:42px; ">指示</th>  
+ <th class=" w-44 h-10 border-red-500 bg-blue-100 border">打赏金币</th>
+ <th class="h-10 border-red-500 bg-blue-100 border " style=" width: 266px;">打赏时间</th>
   </tr>
 </thead>
 <!-- 没有数据 -->
-<!-- <tbody class="noData w-60px h-20px bg-black">
+<!-- <tbody class=" h-24 pt-7 bg-green-100 border-red-500 border border-t-0 text-lg" style=:"width: 745px;">
   <tr>
     <td>没有数据</td>
   </tr>
@@ -30,17 +30,17 @@
 <!-- 我的打赏 -->
 <tbody v-for="item in items" :key="item.message" v-show ="!RewardPage">
   <tr>
-    <td class="indicationData">{{item.message}}</td>
-    <td  class="rewardData">{{item.ss}}</td>
-    <td  class="timeData">{{item.sss}}</td>
+    <td class=" h-10 border-red-500 bg-blue-200 border" style="width: 301px">{{item.message}}</td>
+    <td  class=" w-44 h-10  border-red-500 bg-blue-200 border">{{item.ss}}</td>
+    <td  class=" h-10 border-red-500 bg-blue-200 border" style="width: 266px;">{{item.sss}}</td>
   </tr>
-</tbody>
+</tbody>  
 <!-- 收到的打赏 -->
 <tbody v-for="item in shuju" :key="item.message" v-show="RewardPage">
   <tr>
-    <td class="indicationData">{{item.one}}</td>
-    <td  class="rewardData">{{item.tow}}</td>
-    <td  class="timeData">{{item.san}}</td>
+    <td class="  h-10 border-red-500 bg-blue-200 border" style="width: 301px">{{item.one}}</td>
+    <td  class=" w-44 h-10  border-red-500 bg-blue-200 border">{{item.tow}}</td>
+    <td  class=" h-10 border-red-500 bg-blue-200 border" style="width: 266px;">{{item.san}}</td>
   </tr>
 </tbody>
      </table>
@@ -50,7 +50,7 @@
  </div> -->
       </div>
     </div>
-    <div class="LayoutButton">
+    <div class="LayoutButton h-10" style="width:796px">
     </div>
   </section>
 
@@ -88,85 +88,3 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.rewardLayout {
-  width: 796px;
-  height: 500px;
-  // background: aquamarine;
-  margin: 20px 0px 10px 0px;
-}
-.rewardtop{
-  width:700px;
-   margin: 50px 20px 20px 0px;
-  font-size:14px;
-}
-
-.tableSize{
-  width: 745px;
-  height: 100%;
-}
-.tableIndication{
-  width: 301px;
-  height: 42px;
-   background-color: #DCE7FD; 
-  border-style:solid;
-	border-width:1px;
-  border-color:red;
-}
-.tableGold{
-  width: 178px;
-  height: 42px;
-   background-color: #DCE7FD; 
- border-style:solid;
-	border-width:1px;
-   border-color:red;
-}
-.tableTime{
-  width: 266px;
-  height: 42px;
-   background-color: #DCE7FD; 
-  border-style:solid;
-	border-width:1px;
-   border-color:red;
-}
-.noData{
-  width: 745px;
-  height: 90px;
-  padding-top:30px ;
-  background-color: blue ;
- border-style:solid;
-	border-width:1px;
-   border-color:red;
-   border-top-style:none;
- font-size: 18px;
-} 
-
-.indicationData{
-   width: 301px;
-  height: 42px;
-   background-color: #e9ecef;
-  border-style:solid;
-	border-width:1px;
-  border-color:red;
-}
-.rewardData{
-   width: 178px;
-  height: 42px;
-   background-color: #e9ecef;
-  border-style:solid;
-	border-width:1px;
-   border-color:red;
-}
-.timeData{
-   width: 266px;
-  height: 42px;
-   background-color: #e9ecef;
-  border-style:solid;
-	border-width:1px;
-   border-color:red;
-}
-.LayoutButton {
-  width: 796px;
-  height: 40px;
-}
-</style>
