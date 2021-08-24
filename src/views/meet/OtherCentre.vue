ac9e70a9f306c7d4f55f80c32562bcd0de173ea4
杜萌雨 <dumengyu@zoneyet.com>
2021-08-24 18:13:37 +0800

    对大图片以及大图片旁评论进行了优化

--- src/views/meet/OtherCentre.vue
+++ src/views/meet/OtherCentre.vue
@@ -195,6 +195,7 @@
             <div class="border mt-2 text-left flex" style="">
               <div class="imgboxs" style="overflow: hidden">
                 <img
+                  @click.stop="bigCommentImage = true"
                   src="https://images.gagahi.com/Z-298ee8d8262c451e80eedf09c8d0dabc?imageView2/5/w/123/h/120"
                   alt=""
                   style="
@@ -454,47 +455,279 @@
       </div>
       <!-- 评论图片放大 -->
       <div
-        class="imageTextBig left-1/3"
-        v-if="showTextImg"
+        v-if="bigCommentImage"
         style="
-          z-index: 19891023;
-          width: 560px;
+          z-index: 1;
+          width:860px;
           height: 630px;
           top: 145.5px ;rgba（0，0，0，0．7）;
         "
+        class="left-1/4 fixed"
       >
-        <span
-          class="closedImg w-9 h-9 rounded-full -top-2 -right-3 absolute"
-          @click="closeImg()"
-        ></span>
-        <div class="imageBigContent" id="sellineImg">
-          <div class="imageBigDetail">
-            <div class="imageBigDetailImg">
-              <div class="imageBigDetailImgLeft"></div>
-              <div class="imageBigDetailImgRight"></div>
-              <div class="bigger">原始尺寸</div>
-              <div class="normal hide" data-url data-i>重置</div>
-              <div class="bd slide_bd">
-                <div
-                  class="slide_item"
-                  imgid="61163a5293d24eeba0605320b5ca5830"
-                >
+        <!-- 左侧 -->
+        <div
+          class="imageTextBig"
+          v-if="showTextImg"
+          style="z-index: 19891023; width: 560px; height: 630px"
+        >
+          <div class="imageBigContent" id="sellineImg">
+            <div class="imageBigDetail">
+              <div class="imageBigDetailImg">
+                <div class="imageBigDetailImgLeft"></div>
+                <div class="imageBigDetailImgRight"></div>
+                <div class="bigger">原始尺寸</div>
+                <div class="normal hide" data-url data-i>重置</div>
+                <div class="bd slide_bd">
                   <div
-                    class="js_img_border"
-                    v-for="(item, index) in images"
-                    :key="index"
+                    class="slide_item"
+                    imgid="61163a5293d24eeba0605320b5ca5830"
                   >
-                    <img
-                      :src="item"
-                      class=""
-                      style="position: relative; margin: 0 auto"
-                      alt=""
-                    />
+                    <div
+                      class="js_img_border"
+                      v-for="(item, index) in images"
+                      :key="index"
+                    >
+                      <img
+                        :src="item"
+                        class=""
+                        style="position: relative; margin: 0 auto"
+                        alt=""
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+        <!-- 右侧 -->
+        <div class="imageTextBigRight">
+          <div class="imageTextBigRightDetail">
+            <!-- 右侧顶部说说内容和留言内容 -->
+            <div class="detailTop">
+              <div class="pl-3 pr-2">
+                <!-- 头像昵称时间 -->
+                <div class="flex">
+                  <!-- 头像 -->
+                  <div style="width: 45px; height: 45px">
+                    <img
+                      src="https://images.gagahi.com/A-52b1e4ab64144bd597935df5c8bf96e3"
+                      alt=""
+                      style="
+                        border-radius: 50%;
+                        max-width: 100%;
+                        max-height: 100%;
+                        width: 100%;
+                        height: 100%;
+                      "
+                    />
+                  </div>
+                  <!-- 昵称，时间 -->
+                  <div class="ml-1">
+                    <div
+                      class="text-left"
+                      style="font-size: 14px; color: #2d57a1"
+                    >
+                      Sally
+                    </div>
+                    <div style="font-size: 12px; color: #696969">
+                      2019-05-12 09:41
+                    </div>
+                  </div>
+                </div>
+                <!-- 说说内容 -->
+                <div class="pt-2 text-left">
+                  Hello. I am new here.welcome to chat
+                </div>
+                <!-- 说说和举报图标 -->
+                <div class="pl-2 pr-2" style="padding: 10px">
+                  <div class="flex justify-between">
+                    <div>
+                      <i class="imgLeft ml-5 align-text-top"> </i>
+                      <div
+                        class="imgLeftText ml-2 inline-block"
+                        style="color: #999"
+                      >
+                        {{ commentList.length }}
+                      </div>
+                    </div>
+                    <div>
+                      <a
+                        class="imgRight align-text-top"
+                        @click="warning()"
+                        title="举报"
+                      ></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- 评论所有内容 -->
+              <div v-if="commentList.length > 0">
+                <div
+                  v-for="(item, index) in commentList"
+                  :key="index"
+                  style="
+                    border-top: 1px solid #e9e9e9;
+                    height: auto;
+                    padding: 10px 10px 0px 12px;
+                  "
+                >
+                  <!-- 评论头像昵称文字 -->
+                  <div class="flex mt-2" style="">
+                    <!-- 头像 -->
+                    <div style="width: 30px; height: 30px">
+                      <img
+                        src="https://sources.gagahi.com/3e8b932396c54c7da7fa5407ddb61dc4"
+                        alt=""
+                        style="
+                          border-radius: 50%;
+                          max-width: 100%;
+                          max-height: 100%;
+                          width: 100%;
+                          height: 100%;
+                          cursor: pointer;
+                        "
+                      />
+                    </div>
+                    <div class="ml-1 w-full mb-4">
+                      <!-- 昵称，时间 -->
+                      <div class="text-left -mt-2">
+                        <span style="color: #90a0bc; font-size: 12px"
+                          >{{ item.name }}
+                        </span>
+                        <span
+                          class=""
+                          style="font-size: 12px; color: #ccc; margin-left: 8px"
+                          >{{ item.time }}</span
+                        >
+                      </div>
+                      <!-- 文字 -->
+                      <div class="text-left" style="line-height: 18px">
+                        <span
+                          class="align-middle"
+                          style="font-size: 12px !important"
+                        >
+                          {{ item.content }}
+                        </span>
+                        <!-- 语言选择 -->
+                        <i class="commentEarth relative ml-2 align-middle">
+                          <!-- @mousemove="moveEarth"
+                          @mouseleave="leaveEarth" -->
+                          <div
+                            class="commentEarthLan absolute"
+                            style="top: -117px"
+                          >
+                            <i class="lanI"></i>
+                            <ul
+                              class="lanUl"
+                              style="background-color: #fff; font-size: 14px"
+                            >
+                              <li langs-lang="en">English</li>
+                              <li langs-lang="zh">简体中文</li>
+                              <li langs-lang="cht">繁體中文</li>
+                              <li langs-lang="kor">한국어</li>
+                              <li langs-lang="ru">Pусский</li>
+                              <li langs-lang="de">Deutsch</li>
+                              <li langs-lang="spa">Español</li>
+                              <li langs-lang="jp">日本語</li>
+                            </ul>
+                          </div>
+                        </i>
+                        <!-- 评论图标 -->
+                        <i
+                          class="commentText ml-2 align-middle"
+                          @click.stop="commentShow = !commentShow"
+                        >
+                        </i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- 右侧底部input内容 -->
+            <div style="height: 13px"></div>
+            <!-- 点击评论前 -->
+            <div
+              @click.stop="commentShow = true"
+              v-show="commentShow == false"
+              class="text-left"
+              style="
+                cursor: text;
+                color: #999;
+                height: 30px;
+                padding: 0 20px;
+                line-height: 30px;
+                border: 1px solid #dcdcdc;
+                border-radius: 30px;
+                margin: 0 15px;
+              "
+            >
+              发表评论
+            </div>
+            <!-- 点击评论 -->
+            <div style="margin: 0 15px">
+              <div
+                v-show="commentShow"
+                style="
+                  background: #fff none repeat scroll 0 0;
+                  border-radius: 15px;
+                  position: relative;
+                  z-index: 2;
+                  opacity: 1;
+                "
+              >
+                <textarea
+                  v-model="inputContent1"
+                  id="commentLeave"
+                  name=""
+                  style="
+                    height: 66px;
+                    padding: 10px;
+                    width: 256px;
+                    font-size: 12px;
+                    background: rgba(0, 0, 0, 0) none repeat scroll 0 0;
+                    border: medium none;
+                    color: #333;
+                    outline: medium none;
+                  "
+                  maxlength="240"
+                ></textarea>
+              </div>
+              <div
+                class="text-left flex justify-between"
+                style="
+                  background: #f9f9f9 none repeat scroll 0 0;
+                  height: auto;
+                  padding: 10px 10px 0;
+                "
+              >
+                <span class="" style="color: #999"
+                  >{{ inputContent1.length }}/240</span
+                >
+                <div
+                  class="pr-4 pl-4"
+                  style="
+                    background: #ff625a none repeat scroll 0 0;
+                    color: #fff;
+                    border-radius: 30px;
+                    color: #999;
+                    cursor: pointer;
+                    font-size: 12px;
+                    height: 30px;
+                    text-align: center;
+                  "
+                  @click="submitComment()"
+                >
+                  <i class="submitIcon"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+          <span
+            class="closedImg w-9 h-9 rounded-full -top-2 -right-3 absolute"
+            @click="closeCommentImg()"
+          ></span>
         </div>
       </div>
     </section>
@@ -521,6 +754,8 @@ export default {
       showLanguage: false,
       placeholder: "发布评论",
       inputContent: "",
+      //大图的评论
+      inputContent1: "",
       showicon: true,
       bigImage: false,
       showWarning: false,
@@ -544,6 +779,14 @@ export default {
           name: "可爱小怪咖22",
           time: "03-03 12:12",
           content: "大风吹啊吹321321",
+          number: 323,
+        },
+      ],
+      commentList: [
+        {
+          name: "XiongCB",
+          time: "2019-05-12 15:18",
+          content: "来郑州玩 ",
           number: 323,
         },
       ],
@@ -579,6 +822,8 @@ export default {
       ],
 
       showTextImg: true,
+      commentShow: false,
+      bigCommentImage: false,
     };
   },
   methods: {
@@ -614,6 +859,14 @@ export default {
       if (currentCli) {
         if (!currentCli.contains(event.target)) {
           this.bigImage = false;
+        }
+      }
+    },
+    commentLeave(event) {
+      const currentCli = document.getElementById("commentLeave");
+      if (currentCli) {
+        if (!currentCli.contains(event.target)) {
+          this.commentShow = false;
         }
       }
     },
@@ -653,7 +906,7 @@ export default {
       if (sendValue.value != "") {
         this.contentList.push({
           name: "可爱小怪咖333",
-          time: "03-03 12:12",
+          time: "2019-05-13 15:18",
           content: sendValue.value,
           number: 323,
         });
@@ -663,11 +916,32 @@ export default {
     showIcon() {
       this.showicon = !this.showicon;
     },
+
+    closeCommentImg() {
+      this.bigCommentImage = false;
+    },
+    submitComment() {
+      const sendValue = document.getElementById("commentLeave");
+      console.log(sendValue.value);
+      if (sendValue.value != "") {
+        this.commentList.push({
+          name: "可爱小怪咖333",
+          time: "03-03 12:12",
+          content: sendValue.value,
+          number: 323,
+        });
+      }
+      this.inputContent1 = "";
+    },
+    iCommentClick() {
+      this.commentShow = false;
+    },
   },
 
   mounted() {
     document.addEventListener("click", this.closeSel);
     document.addEventListener("click", this.closeBigImage);
+    document.addEventListener("click", this.commentLeave);
   },
 };
 </script>
@@ -1053,7 +1327,7 @@ export default {
   animation-fill-mode: both;
   -webkit-animation-duration: 0.3s;
   animation-duration: 0.3s;
-  position: absolute;
+  position: fixed;
   .imageBigContent {
     position: relative;
     padding: 10px 25px 25px 25px;
@@ -1182,7 +1456,8 @@ export default {
   background-color: #fff;
   -webkit-background-clip: content;
   box-shadow: 0px 0px 3px rgb(0 0 0 / 20%);
-  border-radius: 15px;
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
   -webkit-animation-fill-mode: both;
   animation-fill-mode: both;
   -webkit-animation-duration: 0.3s;
@@ -1200,7 +1475,8 @@ export default {
       height: 630px;
       width: 100%;
       background: #000;
-      border-radius: 15px;
+      border-top-left-radius: 15px;
+      border-bottom-left-radius: 15px;
       .imageBigDetailImg {
         left: 20px;
         overflow: hidden;
@@ -1280,5 +1556,110 @@ export default {
       }
     }
   }
+}
+.imageTextBigRight {
+  float: right;
+  border-bottom-right-radius: 14px;
+  background: #f9f9f9;
+  width: 300px;
+  height: 630px;
+  position: relative;
+  border-radius: 14px;
+  border-bottom-left-radius: 0;
+  border-top-left-radius: 0;
+  .imageTextBigRightDetail {
+    max-height: 100%;
+    height: 630px;
+    .detailTop {
+      max-height: 494px;
+      padding-top: 16px;
+      background-color: #fff;
+    }
+  }
+}
+.imgLeft {
+  cursor: pointer;
+  background: url("../../assets/images/dynamic.png") no-repeat;
+  display: inline-block;
+  background-position: -26px -48px;
+  height: 16px;
+  width: 19px;
+}
+.imgLeft:hover {
+  background-position: -26px -26px;
+}
+.imgLeftText {
+  font-family: "Microsoft YaHei", "微软雅黑", arial, "HanHei SC", "PingFang SC",
+    "lucida grande", tahoma, verdana, sans-serif;
+  font-size: 14px;
+}
+.imgRight {
+  background: url("../../assets/images/dynamic.png") no-repeat;
+  display: inline-block;
+  background-position: -83px -26px;
+  width: 20px;
+  height: 17px;
+}
+.imgRight a {
+  text-decoration: none;
+  color: #333;
+}
+.commentEarth {
+  cursor: pointer;
+  background: url("../../assets/images/dynamic.png") no-repeat;
+  display: inline-block;
+  background-position: -25px -2px;
+  width: 13px;
+  height: 13px;
+  .commentEarthLan {
+    color: #333;
+    font-style: normal;
+    position: absolute;
+    left: -30px;
+    width: 136px;
+    padding: 10px 5px;
+    border: 1px solid #e9e9e9;
+    border-radius: 5px;
+    background: #fff;
+    z-index: 99;
+    display: none;
+  }
+  .lanI {
+    width: 20px;
+    height: 30px;
+    position: absolute;
+    left: 30px;
+    top: -13px;
+    z-index: 2;
+    background: url("~@images/person/IMicon.png") 0px -75px repeat;
+    top: 100px;
+    background-position: -208px -73px;
+  }
+  .lanUl li {
+    padding: 0 5px;
+    line-height: 22px;
+    float: left;
+    cursor: pointer;
+  }
+}
+.commentEarth:hover {
+  .commentEarthLan {
+    display: block;
+  }
+}
+.commentText {
+  background: url("../../assets/images/dynamic.png") no-repeat;
+  display: inline-block;
+  cursor: pointer;
+  background-position: -45px -3px;
+  width: 15px;
+  height: 12px;
+}
+.submitIcon {
+  background: url("../../assets/images/dynamic.png") no-repeat;
+  display: inline-block;
+  background-position: -251px 8px;
+  height: 24px;
+  width: 14px;
 }
 </style>
